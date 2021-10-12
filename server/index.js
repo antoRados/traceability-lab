6106903c44a128ec2b5cbfd7265c69583eb0ae30
antoRados <antorados@gmail.com>
2021-10-12 17:14:39 -0400

    syntax

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "./server/index.html"))
+    res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
 const SERVER_PORT = 5050
