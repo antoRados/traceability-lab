10cf062757990ff8e4779ac3f1d17866e13ac285
antoRados <antorados@gmail.com>
2021-10-12 15:05:57 -0400

    add .get request to index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const path = require('path')
 
 const app = express();
 
-
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"))
+})
 
 const SERVER_PORT = 5050
 const port = process.env.PORT || SERVER_PORT
